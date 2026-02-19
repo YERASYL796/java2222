5c68cd77a02dc3f139db77acc2a987b208e939b8
YERASYL796 <agabekerasyl2@gmail.com>
2026-02-19 12:07:00 +0500

    m,jh

---
+++ lab1/lab1.java
@@ -0,0 +1,7 @@
+import java.time.LocalTime;
+
+class lab1 {
+    static void main() {
+        System.out.println("Қазіргі уақыт: " + LocalTime.now());
+    }
+}
---
+++ lab5/lab5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class lab5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Санды енгізіңіз: ");
+        double san = scanner.nextDouble();
+
+        if (san > 0) {
+            System.out.println("Сан оң.");
+        } else if (san < 0) {
+            System.out.println("Сан теріс.");
+        } else {
+            System.out.println("Сан нөлге тең.");
+        }
+        scanner.close();
+    }
+}
